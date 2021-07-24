869ef89e79d1b8504b8c657447eb476351ceba04
LucasColaco <lucascolaco18@gmail.com>
2021-07-24 20:25:16 -0300

    Implementação do CRUD de Responsável

--- xtarefas/src/main/java/com/basis/campina/xtarefas/servico/ResponsavelService.java
+++ xtarefas/src/main/java/com/basis/campina/xtarefas/servico/ResponsavelService.java
@@ -1,5 +1,11 @@
 package com.basis.campina.xtarefas.servico;
 
+import com.basis.campina.xtarefas.dominio.Responsavel;
+import com.basis.campina.xtarefas.repositorio.ResponsavelRepository;
+import com.basis.campina.xtarefas.servico.dto.ResponsavelDTO;
+import com.basis.campina.xtarefas.servico.mapper.ResponsavelMapper;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -8,4 +14,27 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @RequiredArgsConstructor
 public class ResponsavelService {
+
+    private final ResponsavelRepository responsavelRepository;
+    private final ResponsavelMapper responsavelMapper;
+
+    public List<ResponsavelDTO> buscar(){
+        return responsavelRepository.findAll().stream().map(responsavelMapper::toDto).collect(Collectors.toList());
+    }
+
+    public ResponsavelDTO buscarPorId(Integer id){
+        Responsavel responsavel = responsavelRepository.findById(id).orElseThrow(()->new RuntimeException("Responsável não encontrado"));
+        return responsavelMapper.toDto(responsavel);
+    }
+
+    public ResponsavelDTO salvar(ResponsavelDTO responsavelDTO){
+        Responsavel obj = responsavelMapper.toEntity(responsavelDTO);
+        obj = responsavelRepository.save(obj);
+        return responsavelMapper.toDto(obj);
+    }
+
+    public void remover(Integer id){
+        buscarPorId(id);
+        responsavelRepository.deleteById(id);
+    }
 }
