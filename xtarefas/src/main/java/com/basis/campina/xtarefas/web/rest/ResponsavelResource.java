2ee8151ef3f6034b4bbcc2f50874faad9088c0fa
LucasColaco <lucascolaco18@gmail.com>
2021-07-24 20:04:57 -0300

    Estrutura geral de Responsável

---
+++ xtarefas/src/main/java/com/basis/campina/xtarefas/web/rest/ResponsavelResource.java
@@ -0,0 +1,11 @@
+package com.basis.campina.xtarefas.web.rest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/responsaveis")
+public class ResponsavelResource {
+}
